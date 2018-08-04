8ae4b4e22fac815dc8b69a3790577418f80e465d
unknown <jessev93@live.ca>
2018-08-04 12:15:38 +0700

    fixed scope issues

--- app.js
+++ app.js
@@ -16,34 +16,34 @@ document.addEventListener("DOMContentLoaded", function (event) {
         list.forEach(function ({ friend }) {
 
             let div1 = document.createElement("div")
-            let p = document.createElement("P")
             let header = document.createElement("h3")
             let name = document.createTextNode(friend)
-            header.appendChild(name)
             div1.appendChild(header)
-            godDiv.appendChild(div1)
-            div1.className = "Friends"
-            let lines;
+            header.appendChild(name)
 
             for (let n = 99; n > 0; n--) {
 
-                let n = 99
+                let p = document.createElement("P")
+
+                let lines;
 
                 let = (minusOne = n - 1)
 
+                if (n > 1) {
+                    lines = document.createTextNode(n + " lines of code in the file, " + n + " lines of code, " + friend + " strikes one out, clears it all out, " + minusOne + " lines of code in the file!")
 
-                if (n > 2) {
-                    (lines = document.createTextNode(n + " lines of code in the file, " + n + " lines of code, " + friend + " strikes one out, clears it all out, " + minusOne + " lines of code in the file!"))
-
-                } else if (n === 2) {
-                    (lines = document.createTextNode(n + " lines of code in the file, " + n + " lines of code, " + friend + " strikes one out, clears it all out, " + minusOne + " lines of code in the file!"))
                 } else {
-                    (lines = document.createTextNode("1 line of code in the file, 1 line of code, " + friend + " strikes one out, clears it all out, no more lines of code in the file!"));
+                    lines = document.createTextNode("1 line of code in the file, 1 line of code, " + friend + " strikes one out, clears it all out, no more lines of code in the file!");
 
                 }
+
+                p.appendChild(lines)
+                div1.appendChild(p)
+                godDiv.appendChild(div1)
+                div1.className = "Friends"
+
             }
-            p.appendChild(lines)
-            div1.appendChild(p)
+
         });
 
     });
